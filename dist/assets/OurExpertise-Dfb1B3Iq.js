e38f278fb41cba78911d201ac33911b3d97218bd
Rutuja kadam <99269092+Rutukadam1910@users.noreply.github.com>
2025-09-19 14:40:45 +0530

    Fix:loading animation changed

---
+++ dist/assets/OurExpertise-Dfb1B3Iq.js
@@ -0,0 +1,31 @@
+import{c as i,r as s,j as e}from"./index-CsNPOCGq.js";/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=[["path",{d:"M16 20V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v16",key:"jecpp"}],["rect",{width:"20",height:"14",x:"2",y:"6",rx:"2",key:"i6l2r4"}]],l=i("briefcase",r);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const d=[["path",{d:"m18 16 4-4-4-4",key:"1inbqp"}],["path",{d:"m6 8-4 4 4 4",key:"15zrgr"}],["path",{d:"m14.5 4-5 16",key:"e7oirm"}]],p=i("code-xml",d);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["path",{d:"m11 17 2 2a1 1 0 1 0 3-3",key:"efffak"}],["path",{d:"m14 14 2.5 2.5a1 1 0 1 0 3-3l-3.88-3.88a3 3 0 0 0-4.24 0l-.88.88a1 1 0 1 1-3-3l2.81-2.81a5.79 5.79 0 0 1 7.06-.87l.47.28a2 2 0 0 0 1.42.25L21 4",key:"9pr0kb"}],["path",{d:"m21 3 1 11h-2",key:"1tisrp"}],["path",{d:"M3 3 2 14l6.5 6.5a1 1 0 1 0 3-3",key:"1uvwmv"}],["path",{d:"M3 4h8",key:"1ep09j"}]],x=i("handshake",h);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=[["path",{d:"M15 14c.2-1 .7-1.7 1.5-2.5 1-.9 1.5-2.2 1.5-3.5A6 6 0 0 0 6 8c0 1 .2 2.2 1.5 3.5.7.7 1.3 1.5 1.5 2.5",key:"1gvzjb"}],["path",{d:"M9 18h6",key:"x1upvd"}],["path",{d:"M10 22h4",key:"ceow96"}]],u=i("lightbulb",m);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const j=[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]],y=i("search",j);/**
+ * @license lucide-react v0.525.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=[["path",{d:"M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z",key:"cbrjhi"}]],v=i("wrench",k),b=[{id:"technical-proficiency",title:"Technical Proficiency",icon:e.jsx(p,{size:56,color:"#31CDF0"})},{id:"industry-experience",title:"Industry Experience",icon:e.jsx(l,{size:56,color:"#F95E00"})},{id:"research-development",title:"Research and Development",icon:e.jsx(y,{size:56,color:"#FCCA00"})},{id:"collaborations-partnership",title:"Collaborations and Partnership",icon:e.jsx(x,{size:56,color:"#0D6EFF"})},{id:"innovative-solutions",title:"Innovative Solutions",icon:e.jsx(u,{size:56,color:"#96E15C"})},{id:"ongoing-support-maintenance",title:"Ongoing Support & Maintenance",icon:e.jsx(v,{size:56,color:"#EC5A83"})}],E=()=>{const[c,a]=s.useState(!1);return s.useEffect(()=>{const t=setTimeout(()=>a(!0),100);return()=>clearTimeout(t)},[]),e.jsxs("section",{className:"expertise-section","aria-label":"Our Expertises",children:[e.jsx("div",{className:`subtitle ${c?"visible":""}`,children:"Where Skill Meets Innovation"}),e.jsx("h2",{className:"heading",children:"OUR EXPERTISES"}),e.jsx("div",{className:"expertise-scroll-wrapper",children:e.jsx("div",{className:"expertise-grid",children:b.map(({id:t,title:o,icon:n})=>e.jsxs("div",{className:"expertise-card",children:[e.jsx("div",{className:"icon-wrapper-expertise",children:n}),e.jsx("div",{className:"expertise-title",children:o})]},t))})})]})};export{E as default};
